d20bf5cb71ef7dde6b97319e64001d8ae06d3e16
Hiald <jonathan.seminario11@gmail.com>
2021-04-07 19:08:32 -0500

    avance

---
+++ frontend_HistorialMedico/frontend_HistorialMedico/Controllers/moduloController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace frontend_SoftColegio.Controllers
+{
+    public class moduloController : Controller
+    {
+        // GET: modulo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
